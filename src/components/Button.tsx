9bb8d9d17db9f8bf793ae2085403bbf7d96931a9
GPivoto <gabrielmpivoto10@gmail.com>
2025-05-10 19:51:22 -0300

    first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+import { ArrowUpRight } from "lucide-react";
+
+interface ButtonProps {
+  text: string;
+}
+
+export function Button({ text }: ButtonProps) {
+  return (
+    <button className="border border-gray-50 py-2 px-8 rounded-full gap-1 flex items-center justify-center text-gray-100 cursor-pointer hover:bg-purple-50 hover:text-white transition-colors duration-300 ease-in-out ">
+      {text}
+      <ArrowUpRight className="w-5 mt-1" />
+    </button>
+  );
+}
