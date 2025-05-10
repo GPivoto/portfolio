bddb2a9b4f94261c08183aa4fce5acb22923b018
GPivoto <gabrielmpivoto10@gmail.com>
2025-05-10 20:23:07 -0300

    style: adjust color palette

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -62,12 +62,12 @@ export function Navbar() {
         </nav>
 
         <div className=" hidden lg:flex items-center space-x-5">
-          <Moon className="text-gray-100/80 cursor-pointer" />
+          <Moon className= " text-gray-100/80 cursor-pointer" />
           <Button text="Connect" />
         </div>
 
         <div className="sm:hidden flex items-center space-x-4">
-          <Moon className="text-gray-100/80 cursor-pointer" />
+          <Moon className="text-black cursor-pointer" />
           <Image
             onClick={() => setVisible(true)}
             src={menuBlack}
@@ -80,7 +80,7 @@ export function Navbar() {
       {/* mobile menu */}
 
       <div
-        className={`md:hidden z-99 absolute top-0 right-0 bottom-0 overflow-hidden bg-[#9c82cb] border-l transition-all duration-500 ${
+        className={`md:hidden z-99 absolute top-0 right-0 bottom-0 overflow-hidden bg-white border-l transition-all duration-500 ${
           visible ? "w-1/2" : "w-0"
         }`}
       >
@@ -88,44 +88,44 @@ export function Navbar() {
           onClick={() => setVisible(false)}
           className="absolute right-6 top-6 cursor-pointer"
         >
-          <X className="w-6 text-white" />
+          <X className="w-6 text-black" />
         </div>
 
         <ul className="mt-20 px-10 space-y-4">
           <li className="hover:-translate-y-1 transition-transform duration-300 ease-in-out">
             <a
               href="#home"
-              className="relative font-semibold text-white transition-all duration-300 ease-in-out hover:text-white group"
+              className="relative font-semibold text-gray-100 transition-all duration-300 ease-in-out hover:text-purple-100 group"
             >
               Home
-              <span className="absolute left-0 -bottom-1 h-[2px] w-0 bg-white transition-all duration-300 group-hover:w-full"></span>
+              <span className="absolute left-0 -bottom-1 h-[2px] w-0 bg-purple-100 transition-all duration-300 group-hover:w-full"></span>
             </a>
           </li>
           <li className="hover:-translate-y-1 transition-transform duration-300 ease-in-out">
             <a
               href="#about"
-              className="relative font-semibold text-white transition-all duration-300 ease-in-out hover:text-white group"
+              className="relative font-semibold text-gray-100 transition-all duration-300 ease-in-out hover:text-purple-100 group"
             >
               About me
-              <span className="absolute left-0 -bottom-1 h-[2px] w-0 bg-white transition-all duration-300 group-hover:w-full"></span>
+              <span className="absolute left-0 -bottom-1 h-[2px] w-0 bg-purple-100 transition-all duration-300 group-hover:w-full"></span>
             </a>
           </li>
           <li className="hover:-translate-y-1 transition-transform duration-300 ease-in-out">
             <a
               href="#projects"
-              className="relative font-semibold text-white transition-all duration-300 ease-in-out hover:text-white group"
+              className="relative font-semibold text-gray-100 transition-all duration-300 ease-in-out hover:text-purple-100 group"
             >
               Projects
-              <span className="absolute left-0 -bottom-1 h-[2px] w-0 bg-white transition-all duration-300 group-hover:w-full"></span>
+              <span className="absolute left-0 -bottom-1 h-[2px] w-0 bg-purple-100 transition-all duration-300 group-hover:w-full"></span>
             </a>
           </li>
           <li className="hover:-translate-y-1 transition-transform duration-300 ease-in-out">
             <a
               href="#contact"
-              className="relative font-semibold text-white transition-all duration-300 ease-in-out hover:text-white group"
+              className="relative font-semibold text-gray-100 transition-all duration-300 ease-in-out hover:text-purple-100 group"
             >
               Contact
-              <span className="absolute left-0 -bottom-1 h-[2px] w-0 bg-white transition-all duration-300 group-hover:w-full"></span>
+              <span className="absolute left-0 -bottom-1 h-[2px] w-0 bg-purple-100 transition-all duration-300 group-hover:w-full"></span>
             </a>
           </li>
         </ul>
